8ae198c9a1e6f612c760cf73dc306a3b7c4bc54c
nansenho <nansen.ho@foxmail.com>
2021-12-05 18:53:55 +0800

    安装 parcel，解决 parcel 的两个报错；重构代码；

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import Button from './y-button.vue'
+
+Vue.component('y-button', Button)
+
+new Vue({
+    el: '#app',
+});
---
+++ src/y-button.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="y-button">按钮</button>
+</template>
+
+<script>
+export default {
+  name: 'y-button',
+};
+</script>
+
+<style scoped lang="scss">
+.y-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 2em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+}
+
+.y-button:hover {
+  background-color: var(--button-active-bg);
+}
+
+.y-button:active {
+  background-color: var(--button-active-bg);
+}
+
+.y-button:focus {
+  outline: none;
+}
+</style>
