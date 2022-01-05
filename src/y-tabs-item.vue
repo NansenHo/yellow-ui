6cc7a71f121d01ae9ab1bdf6c48afe5ae2d40d67
henansong <github@nansenho.com>
2022-01-05 16:28:25 +0800

    tabs 可以切换

--- src/y-tabs-item.vue
+++ src/y-tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item">
+  <div class="tabs-item" @click="xxx" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -7,16 +7,55 @@
 <script>
 export default {
   name: "y-tabs-item",
+  inject: ['eventBus'],
+  data() {
+    return {
+      active: false,
+    }
+  },
   props: {
     disabled: {
       type: Boolean,
       default: false,
-
+    },
+    name: {
+      type: [String, Number],
+      required: true
     }
-  }
+  },
+  computed: {
+    classes() {
+      return {
+        active: this.active
+      }
+    }
+  },
+  created() {
+    // item 本身也监听这个事件
+    this.eventBus.$on('update:selected', (name) => {
+      if (name === this.name) {
+        this.active = true
+      } else {
+        this.active = false
+      }
+    })
+  },
+  methods: {
+    xxx() {
+      // 当点击了该item，那就会更新 selected 为 this.name
+      this.eventBus.$emit('update:selected', this.name)
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+.tabs-item {
+  flex-shrink: 0;
+  padding: 0 2em;
 
+  &.active {
+    background: red;
+  }
+}
 </style>
